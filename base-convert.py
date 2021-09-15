feaa0ebd4012000a17a8cad034711fba738fdac4
Andrew Byrd <andrewbyrd256@gmail.com>
2021-09-15 16:34:14 -0500

    Added comments

--- base-convert.py
+++ base-convert.py
@@ -1,10 +1,12 @@
 """Returns the given decimal number expressed in base"""
 def to_base(decimal, base):
     digits = []
+    # Standard algorithm for base conversion--continuously divide by the target base
+    # and save the remainders to obtain the representation in the new base
     while (decimal > 0):
         digits.append(decimal % base)
         decimal = decimal // base
-    digits.reverse()
+    digits.reverse()  #algorithm calculates digits from least to most significant (ie right to left)
     return "".join(map(str, digits))
 
 
