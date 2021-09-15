61fdd783b137090f2e5b4432287f80fa26e979d9
Andrew Byrd <andrewbyrd256@gmail.com>
2021-09-14 19:48:35 -0500

    Add a basic base->decimal function

--- base-convert.py
+++ base-convert.py
@@ -10,4 +10,11 @@ def to_base(decimal, base):
 
 """Returns the decimal representation of the input number"""
 def to_decimal(input, base):
-    pass
+    digits = list(str(input))
+    digits.reverse()  #so we can start with the 1s place and work upwards
+    result = 0
+    exp = 0
+    for digit in digits:
+        result += int(digit) * base**exp
+        exp += 1
+    return result
